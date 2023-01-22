d59fe06ea2ddd88c26dd89b2e174978d173a24ad
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-22 11:32:35 +0530

    added puvi font

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../src/views/ejs"));
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.render("index", { variableName: "Hello World!" });
 });
