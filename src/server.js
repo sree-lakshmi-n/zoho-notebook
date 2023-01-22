5b32435929930dbb4d1c05a23d3fff8d568fb337
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-22 09:03:58 +0530

    rendering index.ejs

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../src/views/ejs"));
+
+app.get("/", (req, res) => {
+  res.render("index", { variableName: "Hello World!" });
+});
+
+app.listen(port, () => {
+  console.log("Listening to port: ", port);
+});
